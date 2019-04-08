e29e851e8b348e9fa38cd28bb1a89fa2d16e26a9
lizhenyu <419090677@qq.com>
2019-04-08 21:15:22 +0800

    test

--- dist/app.js
+++ dist/app.js
@@ -2,26 +2,40 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
-    result["default"] = mod;
-    return result;
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const test = __importStar(require("./controller/test"));
-const test1_1 = require("./controller/test1");
-const test2_1 = __importDefault(require("./controller/test2"));
-const test3 = __importStar(require("./controller/test3"));
-var app = express_1.default();
-test.output();
-var newtset1 = new test1_1.test1();
-newtset1.test1Print();
-var test2new = new test2_1.default();
-test2new.printTest2();
-var test3new = new test3.test1();
-test3new.test1Print();
-console.log("this is a test");
+const http_1 = __importDefault(require("http"));
+const socket_io_1 = __importDefault(require("socket.io"));
+const app = express_1.default();
+const server = http_1.default.createServer(app);
+const io = socket_io_1.default(server);
+const router = express_1.default.Router();
+var port = 3000;
+app.use(express_1.default.static('./bin/'));
+router.get('/', (res, req) => {
+    console.log("this is a render test");
+    req.render("index.html");
+});
+io.on("connection", function (socket) {
+    socket.on("socketIoTest", function (data) {
+        console.log(data);
+        socket.emit("ServerSendMsg", data);
+    });
+});
+server.listen(port, function () {
+    console.log('Server listening at port %d', port);
+});
+// import * as test from "./controller/test";
+// import {test1} from "./controller/test1";
+// import test2 from "./controller/test2";
+// import * as test3 from "./controller/test3";
+// var app = express();
+// test.output();
+// var newtset1 = new test1();
+// newtset1.test1Print();
+// var test2new = new test2();
+// test2new.printTest2();
+// var test3new = new test3.test1();
+// test3new.test1Print();
+// console.log("this is a test");
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,43 @@
 import express from "express";
-import * as test from "./controller/test";
-import {test1} from "./controller/test1";
-import test2 from "./controller/test2";
-import * as test3 from "./controller/test3";
-var app = express();
-test.output();
-var newtset1 = new test1();
-newtset1.test1Print();
-var test2new = new test2();
-test2new.printTest2();
-var test3new = new test3.test1();
-test3new.test1Print();
-console.log("this is a test");
+import http from "http";
+import socketIo from "socket.io";
+import path from "path";
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+const router = express.Router();
+var port = 3000;
+
+app.use(express.static('./bin/'));
+
+router.get('/', (res,req) =>{
+    console.log("this is a render test");
+    req.render("index.html");
+})
+
+io.on("connection", function(socket){
+    socket.on("socketIoTest", function(data){
+       console.log(data);
+       socket.emit("ServerSendMsg", data);
+    });
+ })
+
+ server.listen(port, function(){
+     console.log('Server listening at port %d', port)
+ })
+
+// import * as test from "./controller/test";
+// import {test1} from "./controller/test1";
+// import test2 from "./controller/test2";
+// import * as test3 from "./controller/test3";
+// var app = express();
+// test.output();
+// var newtset1 = new test1();
+// newtset1.test1Print();
+// var test2new = new test2();
+// test2new.printTest2();
+// var test3new = new test3.test1();
+// test3new.test1Print();
+// console.log("this is a test");
 
